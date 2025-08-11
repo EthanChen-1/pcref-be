35ed57de147bb2bfbf4ef728fdf14ed5c8f5db34
Ethan Chen <Ethanchen4@gmail.com>
2025-08-11 19:09:10 -0400

    added env variables

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,7 @@ import { Pool } from "pg";
 
 export const auth = betterAuth({
   database: new Pool({
-    connectionString:
-      "postgres://pcref-db-admin:FluvalBugBites@localhost:5432/pcref-db",
+    connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/pcref-db`,
   }),
   emailAndPassword: {
     enabled: true,
